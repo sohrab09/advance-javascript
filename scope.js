2355797255b2df2ae91b50902b93538a05844d51
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-02-02 23:26:06 +0600

    done

---
+++ scope.js
@@ -0,0 +1,10 @@
+function sum(first, second) {
+    let result = first + second;
+    if (result > 9) {
+        var mood ="happy";
+        mood = "nice";
+        mood = "funny";
+    }
+    console.log(mood);
+    return result;
+}
