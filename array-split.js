ce8fda802edbfc03c2d796af3626cb2233ca7da1
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-02-03 00:07:48 +0600

    Done

---
+++ array-split.js
@@ -0,0 +1,10 @@
+const numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const part = numbers.slice(3, 6);
+
+// console.log(part);
+const removed = numbers.splice(2,6);
+// console.log(removed);
+// console.log(numbers);
+
+const add = numbers.join("Hi");
+console.log(add);
