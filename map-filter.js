f6069cda58a79a05f240056463e301df020c5aef
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-02-02 20:43:20 +0600

    map and filter

---
+++ map-filter.js
@@ -0,0 +1,14 @@
+// numbers = [3, 4, 5, 6, 7, 8, 9];
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+// numbers = [3, 4, 5, 6, 7, 8, 9];
+
+// const result = numbers.filter(x => x < 6);
+// console.log(result);
+
+numbers = [3, 4, 5, 6, 7, 8, 9];
+
+const result = numbers.find(x => x > 4);
+console.log(result);
