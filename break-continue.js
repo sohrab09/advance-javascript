03cc8ad38134d3ea386dae832f59fe99e0794744
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-02-03 01:22:28 +0600

    Done

--- break-continue.js
+++ break-continue.js
@@ -1,17 +1,32 @@
 // const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // for (let i = 0; i < number.length; i++) {
-    
+
 //     if (number[i]>3) {
 //         break;
 //     }
 //     console.log(number[i]);
 // }
 
-const number = [1, -1, -2, 2, 3, -3, -7, -9, 4, 5, 6, 7, 8, 9];
-for (let i = 0; i < number.length; i++) {
-    
-    if (number[i]<0) {
-        continue;
-    }
-    console.log(number[i]);
+// const number = [1, -1, -2, 2, 3, -3, -7, -9, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < number.length; i++) {
+
+//     if (number[i]<0) {
+//         continue;
+//     }
+//     console.log(number[i]);
+// }
+
+// function doSomething(x,y = 4) {
+//     console.log(y);
+// }
+// doSomething(3,2);
+
+if ('false') {
+
+    console.log("I am not false")
+
+} else {
+
+    console.log("I am false")
+
 }
